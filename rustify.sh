13a61f6edfdd6b0e7af60686987dd17cab417399
Alfie John <alfie@alfie.wtf>
2017-08-01 13:08:38 +1000

    Adding racer with Vim integration

--- rustify.sh
+++ rustify.sh
@@ -40,3 +40,23 @@ fi
 if [ ! -d ~/.vim/bundle/rust.vim ]; then
 	git clone --depth=1 https://github.com/rust-lang/rust.vim.git ~/.vim/bundle/rust.vim
 fi
+
+#
+# install racer
+#
+
+if [ ! -f /root/.cargo/bin/racer ]; then
+  cargo install racer
+  rustup component add rust-src
+
+  grep "RUST_SRC_PATH" ~/.bashrc > /dev/null || echo 'export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"' >> ~/.bashrc
+
+  git clone --depth=1 https://github.com/racer-rust/vim-racer.git ~/.vim/bundle/vim-racer
+
+  grep "set hidden"         ~/.vimrc > /dev/null || echo "set hidden"                                            >> ~/.vimrc
+  grep "let g:racer_cmd"    ~/.vimrc > /dev/null || echo 'let g:racer_cmd = "/root/.cargo/bin/racer"'            >> ~/.vimrc
+  grep "rust-def)"          ~/.vimrc > /dev/null || echo "au FileType rust nmap gd <Plug>(rust-def)"             >> ~/.vimrc
+  grep "rust-def-split)"    ~/.vimrc > /dev/null || echo "au FileType rust nmap gs <Plug>(rust-def-split)"       >> ~/.vimrc
+  grep "rust-def-vertical)" ~/.vimrc > /dev/null || echo "au FileType rust nmap gx <Plug>(rust-def-vertical)"    >> ~/.vimrc
+  grep "rust-def-doc)"      ~/.vimrc > /dev/null || echo "au FileType rust nmap <leader>gd <Plug>(rust-def-doc)" >> ~/.vimrc
+fi
